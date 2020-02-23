c8a8ffa1c1c0d55d41339919ac56944ba41b92fb
Marcin Siedlarz <marcin.siedlarz@joymile.com>
2020-02-23 22:07:23 +0100

    refactor: improve admin panel

--- advertising_metrics/admin.py
+++ advertising_metrics/admin.py
@@ -6,13 +6,14 @@ from advertising_metrics.models import DataSource, Campaign, Metric
 
 @admin.register(DataSource)
 class DataSourceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name',)
 
 
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name',)
+
 
 @admin.register(Metric)
 class MetricAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('date', 'data_source', 'campaign', 'clicks', 'impressions')
--- advertising_metrics/models.py
+++ advertising_metrics/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Campaign(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class DataSource(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Metric(models.Model):
